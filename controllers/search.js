f7ee0c8055daad582429b4c4444bc30635c44eab
NQ <bu__dong@163.com>
2015-03-16 00:09:20 +0800

    搜索的sit通过配置获取

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,7 @@
+var config = require('../config');
+
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.baidu.com/s?word=site:qjzd.net ' + q);
+  res.redirect('https://www.baidu.com/s?word=site:' + config.host + " " + q);
 };
