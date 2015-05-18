6dae93bd6025b2861205fee49ae002138573be9a
NQ <bu__dong@163.com>
2015-05-18 18:53:03 +0800

    redis增加密码入参.

--- common/redis.js
+++ common/redis.js
@@ -1,6 +1,6 @@
 var config = require('../config');
 var redis = require('redis');
 
-var client = redis.createClient(config.redis_port, config.redis_host);
+var client = redis.createClient(config.redis_port, config.redis_host, config.redis_options);
 
 exports = module.exports = client;
