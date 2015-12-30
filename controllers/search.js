c88941b6bc1d5f19025455dfe4e3d6eb3f6cd011
NQ <bu__dong@163.com>
2015-12-30 22:52:47 +0800

    当请求为http时,采用百度站内搜索,https时,采用bing搜索

--- controllers/search.js
+++ controllers/search.js
@@ -3,5 +3,5 @@ var config = require('../config');
 exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
-  res.redirect('https://www.baidu.com/s?word=site:' + config.host + " " + q);
+  res.redirect('https://www.bing.com/search?q=site:' + config.host + " " + q);
 };
